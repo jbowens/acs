b4569ab60efec05df9818fc8c7f260782dba6413
Jackson Owens <jackson@chain.com>
2016-11-12 19:32:56 -0800

    calculate house of reps distribution

--- cmd/postmortem/main.go
+++ cmd/postmortem/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/jbowens/postmortem"
@@ -29,12 +30,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Just print the state population estimates to stdout for now.
+	sum := 0
+	stateReps := map[string]int{}
 	statePopulations := map[string]int{}
 	for county, stats := range results {
+		if county.State == "PR" || county.State == "DC" {
+			continue
+		}
+		sum += stats.TotalPopulation.Total
 		statePopulations[county.State] = statePopulations[county.State] + stats.TotalPopulation.Total
 	}
-	for state, pop := range statePopulations {
-		fmt.Printf("%s — %d\n", state, pop)
+
+	representatives := 435
+
+	// Every state gets at least 1 representative.
+	for state := range statePopulations {
+		stateReps[state]++
+		representatives--
+	}
+	for ; representatives > 0; representatives-- {
+		var highestPriority float64
+		var highestPriorityState string
+		for state, n := range stateReps {
+			priority := float64(statePopulations[state]) / math.Sqrt(float64(n)*(float64(n)+1))
+			if priority > highestPriority {
+				highestPriority, highestPriorityState = priority, state
+			}
+		}
+		stateReps[highestPriorityState]++
+	}
+
+	// Print the resulting repsentative counts.
+	for state, reps := range stateReps {
+		fmt.Printf("%02d\t%s\n", reps, state)
 	}
 }
